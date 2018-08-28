6d597372133c3f665a5171a4381925a060319a38
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-08-28 20:04:09 +0200

    S5,L63
    [FA-28]

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,30 +1,19 @@
-// Function constructor
+// Object Create
 
-let john = {
-  name: 'John',
-  yearOfBirth: 1990,
-  job: 'teacher'
+let personProto = {
+  calculateAge: function() {
+    console.log(2018 - this.yearOfBirth);
+  }
 };
 
-console.log(john);
+let john = Object.create(personProto);
 
-const Person = function(name, yearOfBirth, job) {
-  this.name = name;
-  this.yearOfBirth = yearOfBirth;
-  this.job = job;
-};
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
 
-Person.prototype.calculateAge = function() {
-  console.log(2018 - this.yearOfBirth);
-};
-
-Person.prototype.lastName = 'Smith';
-
-john = new Person('John', 1990, 'teacher');
-
-console.log(john);
-
-john.calculateAge();
-
-let jane = new Person('Jane', 1969, 'designer');
-let mark = new Person('Mark', 1948, 'retired');
+let jane = Object.create(personProto, {
+  name: {value: 'Jane'},
+  yearOfBirth: {value: 1969},
+  job: {value: 'designer'}
+});
