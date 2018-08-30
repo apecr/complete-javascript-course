a0746747a61fbcc1d29a6825935512a6478b62cd
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-08-30 14:15:25 +0200

    Include examples of functions
    Function as arguments examples
    
    [FA-28], [S5,L65]

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -13,7 +13,29 @@ john.yearOfBirth = 1990;
 john.job = 'teacher';
 
 let jane = Object.create(personProto, {
-  name: {value: 'Jane'},
-  yearOfBirth: {value: 1969},
-  job: {value: 'designer'}
+  name: { value: 'Jane' },
+  yearOfBirth: { value: 1969 },
+  job: { value: 'designer' }
 });
+
+// Passing functions as arguments
+
+
+const years = [1990, 1965, 1937, 2005, 1998];
+const arrayCalc = (arr, fn) => arr.map(fn);
+
+const calculateAge = yearOfBirth => 2016 - yearOfBirth;
+const isFullAge = age => age >= 18;
+const maxHeartRate = age => (age >= 18 && age <= 81) ? Math.round(206.9 - (0.67 * age)) : -1;
+
+
+const ages = arrayCalc(years, calculateAge);
+console.log(years.map(calculateAge));
+const fullages = arrayCalc(ages, isFullAge);
+console.log(years
+  .map(calculateAge)
+  .map(isFullAge));
+
+console.log(years
+  .map(calculateAge)
+  .map(maxHeartRate));
