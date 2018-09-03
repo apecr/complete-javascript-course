c3e89a23c9184d787684de4c407e89fe019a0f62
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-09-03 11:26:20 +0200

    Include new Item in the data
    [FA-29]

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -3,6 +3,56 @@
 const budgetController = (_ => {
 
   // Some code
+  const Expense = function({ id, description, value }) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  const Income = function({ id, description, value }) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  const data = {
+    items: {
+      expense: [],
+      income: []
+    },
+    totals: {
+      expense: 0,
+      income: 0
+    }
+  };
+
+  return {
+    addItem: ({ type, description, value }) => {
+
+      const getLastId = elements =>
+        elements.length > 0
+          ? elements[elements.length - 1].id + 1
+          : 0;
+
+      if (type === 'expense') {
+        data.items[type].push(new Expense({
+          id: getLastId(data.items[type]),
+          description, value
+        }));
+        return data.items[type][data.items[type].length - 1];
+      } else if (type === 'income') {
+        data.items[type].push(new Income({
+          id: getLastId(data.items[type]),
+          description, value
+        }));
+        return data.items[type][data.items[type].length - 1];
+      }
+    },
+
+    testing: () => {
+      console.log(data);
+    }
+  };
 
 })();
 
@@ -36,18 +86,31 @@ const appController = ((budgetCrl, UICtrl) => {
     console.log(input);
 
     // 2 Add the item to the budget controller.
+    const newItem = budgetCrl.addItem(input);
+
     // 3 Add the new item to the UI.
     // 4 Calculate the budget.
     // 5 Display the budget on the UI.
   };
 
-  document.querySelector(UIController.DOMstrings.inputButton).addEventListener('click', controlAddItem);
+  const setUpEventListeners = _ => {
+    document.querySelector(UIController.DOMstrings.inputButton)
+      .addEventListener('click', controlAddItem);
+    document.addEventListener('keypress', event => {
+      if (event.keyCode === 13 || event.which === 13) {
+        controlAddItem();
+      }
+    });
+  };
 
-
-  document.addEventListener('keypress', event => {
-    if (event.keyCode === 13 || event.which === 13) {
-      controlAddItem();
+  return {
+    init: _ => {
+      console.log('Application started');
+      setUpEventListeners();
     }
-  });
+  };
+
 
 })(budgetController, UIController);
+
+appController.init();
