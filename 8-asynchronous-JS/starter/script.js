a8d4a70c26787f50162941a577b1a2f11fdf1f46
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-09-06 10:27:41 +0200

    Callback hell
    Callback hell example
    
    [FA-31]

--- 8-asynchronous-JS/starter/script.js
+++ 8-asynchronous-JS/starter/script.js
@@ -2,14 +2,23 @@ const getRecipe = () => {
   setTimeout(() => {
     const recipeId = [523, 883, 432, 974];
     console.log(recipeId);
-    setTimeout((id) => {
+
+    setTimeout(id => {
       const recipe = {
         title: 'Fresh tomato pasta',
         publisher: 'Jonas',
         id
       };
       console.log(recipe);
-    }, 1000, recipeId[2]);
+
+      setTimeout(publisher => {
+        const recipe2 = {
+          title: 'Italian Pizza',
+          publisher
+        };
+        console.log(recipe2);
+      }, 1500, recipe.publisher);
+    }, 1500, recipeId[2]);
   }, 1500);
 };
 
