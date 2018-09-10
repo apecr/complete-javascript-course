91602b1202dc724b62a901e1efb6db551e3cdead
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-09-10 08:34:52 +0200

    previous class

--- 8-asynchronous-JS/starter/script.js
+++ 8-asynchronous-JS/starter/script.js
@@ -1,44 +1,61 @@
-const getIds = () => new Promise(resolve => {
-  setTimeout(() => {
-    resolve([523, 883, 432, 974]);
-  }, 1500);
-});
+// const getIds = () => new Promise(resolve => {
+//   setTimeout(() => {
+//     resolve([523, 883, 432, 974]);
+//   }, 1500);
+// });
 
-const getRecipe = recipeID =>
-  new Promise(resolve =>
-    setTimeout(
-      id => resolve({ title: 'Fresh tomato pasta', publisher: 'Joan', id }),
-      1500,
-      recipeID
-    )
-  );
+// const getRecipe = recipeID =>
+//   new Promise(resolve =>
+//     setTimeout(
+//       id => resolve({ title: 'Fresh tomato pasta', publisher: 'Joan', id }),
+//       1500,
+//       recipeID
+//     )
+//   );
 
-const getRelated = publisher => {
-  return new Promise(resolve => {
-    setTimeout(
-      pub => resolve({ title: 'Italian Pizza', publisher: pub })
-      , 1500, publisher
-    );
-  });
+// const getRelated = publisher => {
+//   return new Promise(resolve => {
+//     setTimeout(
+//       pub => resolve({ title: 'Italian Pizza', publisher: pub })
+//       , 1500, publisher
+//     );
+//   });
+// };
+
+// // getIds()
+// //   .then(ids => ids[2])
+// //   .then(getRecipe)
+// //   .then(recipe => {
+// //     console.log(recipe);
+// //     return recipe.publisher;
+// //   })
+// //   .then(getRelated)
+// //   .then(console.log);
+
+// const getRecipesAW = async() => {
+//   const ids = await getIds();
+//   console.log(ids);
+//   const recipe = await getRecipe(ids[2]);
+//   console.log(recipe);
+//   const recipeRelated = await getRelated(recipe.publisher);
+//   console.log(recipeRelated);
+// };
+
+// getRecipesAW();
+
+/* global fetch*/
+
+const getWeather = async() => {
+  const weather = await fetch('https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/2487956/');
+  return weather.body;
 };
 
-// getIds()
-//   .then(ids => ids[2])
-//   .then(getRecipe)
-//   .then(recipe => {
-//     console.log(recipe);
-//     return recipe.publisher;
-//   })
-//   .then(getRelated)
-//   .then(console.log);
-
-const getRecipesAW = async() => {
-  const ids = await getIds();
-  console.log(ids);
-  const recipe = await getRecipe(ids[2]);
-  console.log(recipe);
-  const recipeRelated = await getRelated(recipe.publisher);
-  console.log(recipeRelated);
-};
-
-getRecipesAW();
+getWeather()
+  .then(result => {
+    console.log(result);
+    return result.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(console.error);
