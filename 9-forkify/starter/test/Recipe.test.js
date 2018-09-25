8370ff629096e610a88ffb0e0086e113f9a3aa13
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-09-25 16:17:09 +0200

    test(test/Recipe.test.js) parse ingredients
    Parsing ingredients
    
    [FA-32]

---
+++ 9-forkify/starter/test/Recipe.test.js
@@ -0,0 +1,17 @@
+
+import 'babel-polyfill';
+import Recipe from './../lib/js/models/Recipe';
+import {expect} from 'chai';
+
+/*global describe, it*/
+describe('Testing Recipe', () => {
+  it('Should create a recipe', async() => {
+    const recipe1 = new Recipe(2658);
+    await recipe1.getRecipe();
+    expect(recipe1.url).to.be.equal('http://static.food2fork.com/14573f089.jpg');
+    recipe1.parseIngredients();
+
+    recipe1.ingredients.forEach(ing => console.log(ing));
+    expect(recipe1.ingredients[3].includes('(')).to.be.equal(false);
+  });
+});
